bcc7cea9dc82e743f07d2fc537ba8709d858c5fe
unknown <12687@COMPND-DTP-049.IIPL.COM>
2016-04-12 12:30:07 +0530

    New test featiure

---
+++ MvcApplication1/MvcApplication1/Controllers/NewTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class NewTestController : Controller
+    {
+        //
+        // GET: /NewTest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
