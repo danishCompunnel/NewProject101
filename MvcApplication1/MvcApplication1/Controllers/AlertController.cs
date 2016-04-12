07b18bebe61cc5c7893c5d5f3812890243e8249e
unknown <12687@COMPND-DTP-049.IIPL.COM>
2016-04-12 12:28:12 +0530

    Alert module compeled...

---
+++ MvcApplication1/MvcApplication1/Controllers/AlertController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class AlertController : Controller
+    {
+        //
+        // GET: /Alert/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
